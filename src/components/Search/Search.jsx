a6a02244e275812702bf651f4f79ac3388f1e9fc
serzhan181 <serzhan181@outlook.com>
2021-01-08 21:22:30 +0600

    removed search button

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { withTheme } from 'styled-components'
 import { Input } from './Search.style'
 
 const Search = ({ searchQuery, setSearch }) => {
   return (
     <div>
-      <Input
-        value={searchQuery}
-        onChange={(e) => setSearch(e.target.value)}
-        type='text'
-        placeholder='Search music'
-      />
-      <button style={{ backgroundColor: 'beige' }}>
-        <Link to={`/search?q=${searchQuery}`}>Search</Link>
-      </button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          window.location.replace(`/search?q=${searchQuery}`)
+        }}
+      >
+        <Input
+          value={searchQuery}
+          onChange={(e) => setSearch(e.target.value)}
+          type='text'
+          placeholder='Search music'
+        />
+      </form>
     </div>
   )
 }
