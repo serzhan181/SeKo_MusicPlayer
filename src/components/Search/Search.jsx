8ef3a9a21fa4ae07c7af9030ecf42250561018b3
serzhan181 <serzhan181@outlook.com>
2021-01-11 18:57:26 +0600

    doesn't refresh on submit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { withTheme } from 'styled-components'
+import { useHistory } from 'react-router-dom'
 import { Input } from './Search.style'
 
 const Search = ({ searchQuery, setSearch }) => {
+  const history = useHistory()
+
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault()
-          window.location.replace(`/search?q=${searchQuery}`)
+          history.push(`/search?q=${searchQuery}`)
         }}
       >
         <Input
